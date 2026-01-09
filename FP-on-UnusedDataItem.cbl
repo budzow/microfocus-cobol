3fd3510445dcda336f442195cf2889391aaa99d2
budzow <michal.budzowski@sonarsource.com>
2026-01-09 17:04:36 +0100

    adjust comment

--- FP-on-UnusedDataItem.cbl
+++ FP-on-UnusedDataItem.cbl
@@ -3,7 +3,7 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-      * cobol:COBOL.UnusedDataItem raises FP here as CUSTOM-TYPE is used
+      * cobol:COBOL.UnusedDataItem raises FP here as MY-TYPE is used
       * on the line 9. Also MY-TYPE is not a "data item block" as the message
       *	claims, it's a type definition
        01 MY-TYPE TYPEDEF PIC X(5).       
