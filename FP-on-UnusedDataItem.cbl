a0791560d6d930c7af5f45a63e9619f757635b42
budzow <michal.budzowski@sonarsource.com>
2026-01-09 16:09:02 +0100

    adjust variables naming

--- FP-on-UnusedDataItem.cbl
+++ FP-on-UnusedDataItem.cbl
@@ -4,10 +4,11 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
       * cobol:COBOL.UnusedDataItem raises FP here as CUSTOM-TYPE is used
-      * on the line 9
-       01 CUSTOM-TYPE TYPEDEF PIC X(5).
-       01 VAR TYPE CUSTOM-TYPE VALUE "Hello".
-
+      * on the line 9. Also MY-TYPE is not a "data item block" as the message
+      *	claims, it's a type definition
+       01 MY-TYPE TYPEDEF PIC X(5).       
+       01 MY-VAR TYPE MY-TYPE VALUE "Hello".
+	   
        PROCEDURE DIVISION.
-           DISPLAY "VAR: " VAR.
+           DISPLAY "MY-VAR: " MY-VAR.
            STOP RUN.
