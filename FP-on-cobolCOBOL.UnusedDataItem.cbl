bb32e64988ad358d51f6b7447cb2991876934205
budzow <michal.budzowski@sonarsource.com>
2026-01-09 15:49:47 +0100

    Add files via upload

---
+++ FP-on-cobolCOBOL.UnusedDataItem.cbl
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. FP-on-cobol:COBOL.UnusedDataItem.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+      * cobol:COBOL.UnusedDataItem raises FP here as CUSTOM-TYPE is used
+      * on the line 9
+       01 CUSTOM-TYPE TYPEDEF PIC X(5).
+       01 VAR-OF-CUSTOM-TYPE TYPE CUSTOM-TYPE VALUE "Hello".
+
+       PROCEDURE DIVISION.
+           DISPLAY "VAR-OF-CUSTOM-TYPE contains: " VAR-OF-CUSTOM-TYPE
+           STOP RUN.
